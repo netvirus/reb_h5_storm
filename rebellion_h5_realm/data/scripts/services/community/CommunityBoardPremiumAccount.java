b1d5c05ac56e065c6e1a208080b00e8083a23571
netvirus <yariknetvirus@gmail.com>
2020-12-29 16:56:41 +0700

    CB premium added html

--- rebellion_h5_realm/data/scripts/services/community/CommunityBoardPremiumAccount.java
+++ rebellion_h5_realm/data/scripts/services/community/CommunityBoardPremiumAccount.java
@@ -15,6 +15,7 @@ import l2r.gameserver.network.serverpackets.HideBoard;
 import l2r.gameserver.network.serverpackets.PlaySound;
 import l2r.gameserver.network.serverpackets.ShowBoard;
 import l2r.gameserver.network.serverpackets.components.NpcString;
+import l2r.gameserver.utils.TimeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,28 +30,25 @@ public class CommunityBoardPremiumAccount {
     }
 
     public String getAction(Player player, String bypass) {
-        String finalHtml = "";
+        String html = "";
         if (bypass.startsWith("list")) {
-            String html = HtmCache.getInstance().getNotNull(Config.BBS_HOME_DIR + "pages/premium/index.htm", player);
+            html = HtmCache.getInstance().getNotNull(Config.BBS_HOME_DIR + "pages/premium/index.htm", player);
             StringBuilder builder = new StringBuilder();
             Map<Integer, PremiumBonus> _premiumBonuses = PremiumSystemOptionsData.getInstance().getPremiumBonusList();
             _premiumBonuses.forEach((k, v) -> {
                 builder.append("<tr>");
                 builder.append("<td width=\"32\"><img src=\"").append(v.getBonusIconName()).append("\" width=\"32\" height=\"32\"></td>");
                 builder.append("<td><font color=FF6600 name=\"CreditTextNormal\">").append(v.getBonusName()).append("</font></td>");
-                builder.append("<td><button value=\"Подробнее\" action=\"bypass -h premium_show_").append(v.getBonusId()).append("\" width=65 height=22 back=\"L2UI_CT1.Button_DF_Down\" fore=\"L2UI_CT1.Button_DF\"></td>");
+                builder.append("<td><button value=\"Подробнее\" action=\"bypass -h premium_show_").append(v.getBonusId()).append("\" width=75 height=22 back=\"L2UI_CT1.Button_DF_Down\" fore=\"L2UI_CT1.Button_DF\"></td>");
                 builder.append("</tr>");
             });
-            finalHtml = html.replace("{list}", builder.toString());
+            html = html.replace("{list}", builder.toString());
         } else if (bypass.startsWith("show_")) {
             String bonusId = bypass.substring(5).trim();
-            String html = "Ошибка загрузки премиум системы. Сообщите администратору!";
             PremiumBonus premium = PremiumSystemOptionsData.getInstance().findById(Integer.parseInt(bonusId));
             if (premium != null) {
-                if (!player.getDoublePremiumState())
-                {
-                    if ((player.getPremiumMainTypeState() && !premium.isBonusMain()) || (player.getPremiumSecondTypeState() && premium.isBonusMain()))
-                    {
+                if (!player.getDoublePremiumState()) {
+                    if ((player.getPremiumMainTypeState() && !premium.isBonusMain()) || (player.getPremiumSecondTypeState() && premium.isBonusMain())) {
                         String itemName = ItemsDAO.getInstance().getItemsByItemId(premium.getBonusItemId()).stream().findFirst().get().getName();
                         html = HtmCache.getInstance().getNotNull(Config.BBS_HOME_DIR + "pages/premium/detail.htm", player);
                         String price = premium.getBonusItemAmount() + " " + itemName;
@@ -104,45 +102,68 @@ public class CommunityBoardPremiumAccount {
                         button += "<button value=\"Подключить\" action=\"bypass -h premium_buy_";
                         button += premium.getBonusId();
                         button += "\" back=\"l2ui_ct1.button.button_df_small_down\" fore=\"l2ui_ct1.button.button_df_small\" width=\"250\" height=\"30\" />";
-                        finalHtml = html.replace("{buy}", button);
+                        html = html.replace("{buy}", button);
                     } else {
-                        finalHtml = HtmCache.getInstance().getNotNull(Config.BBS_HOME_DIR + "pages/premium/alreadyhaveall.htm", player);
+                        html = HtmCache.getInstance().getNotNull(Config.BBS_HOME_DIR + "pages/premium/thesame.htm", player);
                     }
                 } else {
-                    finalHtml = HtmCache.getInstance().getNotNull(Config.BBS_HOME_DIR + "pages/premium/alreadyhaveall.htm", player);
+                    html = HtmCache.getInstance().getNotNull(Config.BBS_HOME_DIR + "pages/premium/alreadyhaveall.htm", player);
                 }
-            }
-            ShowBoard.separateAndSend(finalHtml, player);
-        } else if (bypass.startsWith("buy_")) {
+                html = html.replace("{bonus_title}", premium.getBonusName());
+                // Not have
+                html = html.replace("{xp_n}", String.valueOf(player.getRateExp()));
+                html = html.replace("{sp_n}", String.valueOf(player.getRateSp()));
+                html = html.replace("{drop_n}", String.valueOf(player.getRateItems()));
+                html = html.replace("{drop_chance_n}", "x");
+                html = html.replace("{drop_amount_n}", "x");
+                html = html.replace("{spoil_n}", String.valueOf(player.getRateSpoil()));
+                html = html.replace("{spoil_chance_n}", "x");
+                html = html.replace("{spoil_amount_n}", "x");
+                html = html.replace("{adena_n}", String.valueOf(player.getRateAdena()));
+                html = html.replace("{weight_n}", String.valueOf(player.getWeightPercents()));
+                html = html.replace("{craft_n}", "x");
+                html = html.replace("{m_craft_n}", "x");
+                html = html.replace("{extract_n}", "x");
+                html = html.replace("{manor_n}", String.valueOf(Config.RATE_MANOR));
+                html = html.replace("{quest_n}", String.valueOf(Config.RATE_QUESTS_DROP));
+                html = html.replace("{quest_reward_n}", String.valueOf(Config.RATE_QUESTS_REWARD));
+                html = html.replace("{pet_xp_n}", "x");
+                html = html.replace("{raid_drop_chance_n}", String.valueOf(Config.RATE_DROP_RAIDBOSS));
+                html = html.replace("{raid_drop_amount_n}", "x");
+                html = html.replace("{herb_drop_chance_n}", String.valueOf(Config.RATE_DROP_HERBS));
+                html = html.replace("{herb_drop_amount_n}", "x");
+                html = html.replace("{time}", TimeUtils.formatTime((int) premium.getBonusDuration()));
+        }
+        ShowBoard.separateAndSend(html, player);
+    } else if(bypass.startsWith("buy_")) {
+        if (!player.getDoublePremiumState()) {
             player.sendPacket(new HideBoard());
             String bonusId = bypass.substring(4).trim();
             PremiumBonus premium = PremiumSystemOptionsData.getInstance().findById(Integer.parseInt(bonusId));
             if (premium != null) {
                 ItemInstance item = player.getInventory().getItemByItemId(premium.getBonusItemId());
                 int amountItems = premium.getBonusItemAmount();
-                if (item.getCount() >= amountItems)
-                {
+                if (item.getCount() >= amountItems) {
                     long bonusPeriod = (System.currentTimeMillis() / 1000) + premium.getBonusDurationFromProfile();
                     PremiumSystemDAO.getInstance().insert(player.getObjectId(), Integer.parseInt(bonusId), bonusPeriod);
                     PremiumSystemManager.getInstance().enablePremiumStatusFromCommunityBoardPremiumAccount(player);
                     _log.info("Player: " + player.getName() + " has activated " + premium.getBonusName());
                     player.getInventory().destroyItem(item, amountItems);
-                }
-                else
-                {
+                } else {
                     player.sendPacket(new ExShowScreenMessage(NpcString.NONE, 5000, ExShowScreenMessage.ScreenMessageAlign.TOP_CENTER, "Нет нужного кол-ва предметов для покупки!"));
                     player.sendPacket(PlaySound.BROKEN_KEY);
                 }
             }
         }
-        return finalHtml;
     }
+        return html;
+}
 
     public static CommunityBoardPremiumAccount getInstance() {
         return CommunityBoardPremiumAccount.SingletonHolder.INSTANCE;
     }
 
-    private static class SingletonHolder {
-        protected static final CommunityBoardPremiumAccount INSTANCE = new CommunityBoardPremiumAccount();
-    }
+private static class SingletonHolder {
+    protected static final CommunityBoardPremiumAccount INSTANCE = new CommunityBoardPremiumAccount();
+}
 }
