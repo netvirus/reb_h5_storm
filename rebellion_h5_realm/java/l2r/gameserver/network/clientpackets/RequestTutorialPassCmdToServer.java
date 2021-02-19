1348b49155aeb38539717f87b05fc8d2f8ba867e
netvirus <yariknetvirus@gmail.com>
2021-02-19 12:26:11 +0700

    Fixed some bug

--- rebellion_h5_realm/java/l2r/gameserver/network/clientpackets/RequestTutorialPassCmdToServer.java
+++ rebellion_h5_realm/java/l2r/gameserver/network/clientpackets/RequestTutorialPassCmdToServer.java
@@ -5,7 +5,6 @@ import l2r.gameserver.achievements.Achievements;
 import l2r.gameserver.instancemanager.QuestManager;
 import l2r.gameserver.model.Player;
 import l2r.gameserver.model.quest.Quest;
-import l2r.gameserver.randoms.PvPCharacterIntro;
 
 public class RequestTutorialPassCmdToServer extends L2GameClientPacket
 {
@@ -31,16 +30,16 @@ public class RequestTutorialPassCmdToServer extends L2GameClientPacket
 		if(tutorial != null)
 			player.processQuestEvent(tutorial.getName(), _bypass, null);
 		
-		if (_bypass.startsWith("emailvalidation") && Config.ENABLE_EMAIL_VALIDATION)
-		{
-			String[] cm = _bypass.split(" ");
-			
-			if (cm.length < 3)
-			{
-				player.sendMessage("Please fill all required fields.");
-				return;
-			}
-		}
+//		if (_bypass.startsWith("emailvalidation") && Config.ENABLE_EMAIL_VALIDATION)
+//		{
+//			String[] cm = _bypass.split(" ");
+//
+//			if (cm.length < 3)
+//			{
+//				player.sendMessage("Please fill all required fields.");
+//				return;
+//			}
+//		}
 		else if (_bypass.startsWith("_bbs_achievements") && Config.ENABLE_ACHIEVEMENTS)
 		{
 			String[] cm = _bypass.split(" ");
@@ -67,9 +66,9 @@ public class RequestTutorialPassCmdToServer extends L2GameClientPacket
 			Achievements.getInstance().usebypass(player, _bypass, cm);
 			return;
 		}
-		else if (_bypass.startsWith("_pvpcharintro"))
-		{
-			PvPCharacterIntro.getInstance().bypassIntro(player, _bypass);
-		}
+//		else if (_bypass.startsWith("_pvpcharintro"))
+//		{
+//			PvPCharacterIntro.getInstance().bypassIntro(player, _bypass);
+//		}
 	}
 }
